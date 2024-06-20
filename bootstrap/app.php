bf185b1624f1a085c9e66f99c2cdbc5d50f84752
Marcel Caraballo <marcel.caraballo@pradtke.de>
2024-06-20 16:05:58 +0200

    Git merge fuckup everything but fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,16 +7,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-<<<<<<< HEAD
-=======
         api: __DIR__.'/../routes/api.php',
->>>>>>> main
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-<<<<<<< HEAD
-=======
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
@@ -25,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
->>>>>>> main
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {

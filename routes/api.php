3025eace29c0636101c2b725bc884b24c640b823
Marcel Caraballo <marcel.caraballo@pradtke.de>
2024-06-20 14:56:15 +0200

    auth removed

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\EmployeeController;
 
-use App\Http\Controllers\AuthController;
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/employee', [EmployeeController::class, 'store']);
-    Route::get('/employee', [EmployeeController::class, 'index']);
-    Route::get('/employee/{id}', [EmployeeController::class, 'show']);
-    Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
-});
+Route::post('/employee', [EmployeeController::class, 'store']);
+Route::get('/employee', [EmployeeController::class, 'index']);
+Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);

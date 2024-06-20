9aa2c8db0aabef83ec3583fbd5615237230c7a07
Marcel Caraballo <marcel.caraballo@pradtke.de>
2024-06-20 14:44:34 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\EmployeeController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/employee', [EmployeeController::class, 'store']);
+    Route::get('/employee', [EmployeeController::class, 'index']);
+    Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+    Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
 });
